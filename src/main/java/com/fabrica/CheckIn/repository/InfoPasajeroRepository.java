fb1fd313b7ef75d2de8ac63e50ce51a2133a87dc
juandcalderon16 <106625250+juandcalderon16@users.noreply.github.com>
2024-11-15 16:04:41 -0500

    Delete src directory

--- src/main/java/com/fabrica/CheckIn/repository/InfoPasajeroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fabrica.CheckIn.repository;
-
-import com.fabrica.CheckIn.model.InfoPasajero;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InfoPasajeroRepository extends JpaRepository<InfoPasajero, Long> {
-}
--- src/main/java/com/fabrica/CheckIn/repository/TarjetaEmbargueRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fabrica.CheckIn.repository;
-
-import com.fabrica.CheckIn.model.TarjetaEmbargue;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TarjetaEmbargueRepository extends JpaRepository<TarjetaEmbargue, Long> {
-}
